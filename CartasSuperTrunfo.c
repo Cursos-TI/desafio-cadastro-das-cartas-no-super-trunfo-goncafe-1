19b17e3c71e77e84fa49221d64ff275d5a9e6621
goncafe-1 <goncalves.ribeirofe@gmail.com>
2025-03-17 01:29:19 +0000

    enviar para faculdade para correção

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,7 @@ int main (){
     int estado;
     char Inicial = 'P';
     char nome [6] = "Paraná";
+    
 
      char codigo [3];
      printf(" A01\n");
@@ -14,14 +15,19 @@ int main (){
       int habitantes;
       float habitantes = 11.824;
       
-      printf("")
-
       float quilometragem =19.507;
       
       float PIB=665.6;
 
       int pontosturisticos;
       int pontos =18;
+
+       printf("P: %C Paraná\n");
+       printf(" habitantes :%d 11.824\n");
+       printf(" quilometragem : %i 19.507\n");
+       printf(" PIB :%i 665.6\n");
+       printf("pontos turisticos :%d 18\n");
+
 
       printf("Carta 2\n");
         
@@ -44,6 +50,14 @@ int main (){
        int pontosturistcos;
        int pontosturisticos =25;
 
+       printf("A : %C Acre\n");
+       printf("habitantes : %d830.18\n");
+       printf("Quilometragem: %i 164.123\n");
+       printf("PIB:%I 23.6\n");
+       printf("Pontos turisticos : %d 25\n");
+
+       return 0;
+       
     
     
 
