007fb1919c480d28cc6b8eff2a7671e49ebfafc0
goncafe-1 <goncalves.ribeirofe@gmail.com>
2025-03-17 01:06:41 +0000

    começo de codificação

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
 
 int main (){
-    printf("Cartas Sper Trunfo!\n");
-    prinf("Novo commit\n");
+    printf(" Cartas Super Trunfo\n");
+    printf("Carta 1\n");
+
+    int estado;
+    char Inicial = 'P';
+    char nome [6] = "Paraná";
+
+     char codigo [3];
+     printf(" A01\n");
+
+      int habitantes;
+      float habitantes = 11.824;
+      
+      printf("")
+
+      float quilometragem =19.507;
+      
+      float PIB=665.6;
+
+      int pontosturisticos;
+      int pontos =18;
+
+      printf("Carta 2\n");
+        
+      int estado;
+       char inicial = 'A';
+       char nome [3] = "Acre";
+
+       char codigo [3];
+       printf("carta B02\n");
+        
+       int habitantes;
+       float habitantes =830.18;
+
+       float quilometragem;
+       float quilometragem =164.123;
+ 
+       float PIB;   
+       float PIB =23.6;
+
+       int pontosturistcos;
+       int pontosturisticos =25;
+
+    
+    
+
+
+
 }
